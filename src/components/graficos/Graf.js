5d3d16654941b862a401aa53865372252d3d15f8
unknown <andrelopessfla@gmail.com>
2020-05-19 18:55:34 -0300

    testando o graf

--- src/components/graficos/Graf.js
+++ src/components/graficos/Graf.js
@@ -1,15 +1,12 @@
 import React, { Component } from 'react';
+import './Graf.css';
 
 
 export default class Graf extends Component {
     render() {
         return (
-            <div>
-                <div className="card">
-                     <div className="card-body">
-
-                     </div>
-                </div>
+            <div className="graf">
+               
                
             </div>
         )
--- src/components/menu/Menu.js
+++ src/components/menu/Menu.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import './menu.css';
 import Bar from '../bar/Bar';
+import Graf from '../graficos/Graf';
 
 
 export default class Menu extends Component {
@@ -18,7 +19,9 @@ export default class Menu extends Component {
                     <div className="link"><li className="nav-item"><Link to="/card" id="menuli">Cards </Link>  </li> </div> 
                     <div className="link"><li className="nav-item"><Link to="/data" id="menuli">Dados </Link>  </li> </div> 
                     </ul>
-                </div>  
+                    
+                </div>
+                 <Graf /> 
             </div>
            
         )
